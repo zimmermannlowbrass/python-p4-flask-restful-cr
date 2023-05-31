4f18a3c453253fa4211f63125978a3f499813ef9
James Aidan Zimmermann <zimmermann.lowbrass@gmail.com>
2023-05-31 08:37:41 -0500

    completed labwork

--- server/app.py
+++ server/app.py
@@ -16,8 +16,70 @@ db.init_app(app)
 
 api = Api(app)
 
+class Newsletters(Resource):
+
+    def get(self):
+
+        response_dict_list = [n.to_dict() for n in Newsletter.query.all()]
+
+        response = make_response(
+            response_dict_list,
+            200
+        )
+
+        return response
+    
+    def post(self):
+
+        new_record = Newsletter(
+            title=request.form['title'],
+            body=request.form['body']
+        )
+
+        db.session.add(new_record)
+        db.session.commit()
+
+        response_dict = new_record.to_dict()
+
+        response = make_response(
+            response_dict,
+            201
+        )
+        return response
+    
+api.add_resource(Newsletters, '/newsletters')
+
 class Home(Resource):
-    pass
+    
+    def get(self):
+
+        response_dict = {
+            "message": "Welcome to the Newsletter RESTful API",
+        }
+
+        response = make_response(
+            response_dict,
+            200
+        )
+
+        return response
+    
+api.add_resource(Home, '/')
+
+class NewsletterByID(Resource):
+
+    def get(self, id):
+
+        response_dict = Newsletter.query.filter_by(id=id).first().to_dict()
+
+        response = make_response(
+            response_dict,
+            200
+        )
+
+        return response
+
+api.add_resource(NewsletterByID, '/newsletters/<int:id>')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
